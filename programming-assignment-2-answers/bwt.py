472a024363cfdf0733c8346896d0290ca6d84d9e
Harris <Harris.Brandon@bls.gov>
2018-05-11 16:14:35 -0400

    Passed Ex2-1

--- programming-assignment-2-answers/bwt.py
+++ programming-assignment-2-answers/bwt.py
@@ -2,7 +2,8 @@
 import sys
 
 def BWT(text):
-    return ""
+    matrix = sorted(text[i:] + text[:i] for i in range(len(text)))
+    return "".join([row[-1] for row in matrix])
 
 if __name__ == '__main__':
     text = sys.stdin.readline().strip()
